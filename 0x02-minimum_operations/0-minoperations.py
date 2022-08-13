5aa9b34f6ec929bf4fa1953d913c4c45cf0c8c33
prototypedave <popupgfj@gmail.com>
2022-08-13 01:55:07 +0000

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+"""
+   function to calculate minimum operations needed to result
+   in exactly n H characters in the file.
+"""
+
+
+def minOperations(n):
+    nOpe = 0
+    minOpe = 2
+    while n > 1:
+        while n % minOpe == 0:
+            nOpe += minOpe
+            n /= minOpe
+        minOpe += 1
+    return nOpe
