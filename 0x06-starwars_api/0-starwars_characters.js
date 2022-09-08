c72b7c0556cfa2781b983f74ee41e9f91abc9904
protottypedave <popupgfj@gmail.com>
2022-09-08 20:34:07 +0000

    starwars api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+/*  Star Wars Characters */
+const apiUrl = "https://alx-intranet.hbtn.io/films/";
+const request = require('request')
+const movieId = process.argv[2];
+/* request from api */
+request(apiUrl + movieId, (error, response, body) => {
+  if (error) throw error;
+  const characters = JSON.parse(body).characters;
+  showNames(characters);
+});
+/* display names */
+const showNames = (names, i = 0) => {
+  if (i === names.length) return;
+  request(names[i], (error, response, body) => {
+    if (error) throw error;
+    console.log(JSON.parse(body).name);
+    showNames(names, i + 1);
+  });
+};
