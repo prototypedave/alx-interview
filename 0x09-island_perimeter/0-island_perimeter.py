eabe2af7adb4809a0a89895b45b27554293c9f3c
prototypedave <popupgfj@gmail.com>
2022-09-30 02:11:37 +0000

    island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,24 @@
+#!usr/bin/python3
+"""
+Island Perimeter Function
+"""
+
+def island_perimeter(grid):
+    """
+    calculates the distance around an island if any
+    """
+    def edges(matrix):
+        """ detect number of edges along horizontal direction """
+        count = 0
+        for row in matrix:
+            row = [0] + row + [0]
+            for i in range(1, len(row)):
+                count += row[i] != row[i-1]
+        return count
+
+    tgrid = [[] for _ in range(len(grid[0]))]
+    for row in grid:
+        for i, item in enumerate(row):
+            tgrid[i].append(item)
+
+    return edges(grid) + edges(tgrid)
